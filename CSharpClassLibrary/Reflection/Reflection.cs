a42d173505a85e5f87c2d2bc8f1bbece019623a3
Raymond Lei <Raymond.Lei@darkmatter2bd.com>
2021-11-05 12:23:47 -0400

    changes

--- CSharpClassLibrary/Reflection/Reflection.cs
+++ CSharpClassLibrary/Reflection/Reflection.cs
@@ -23,18 +23,20 @@ namespace CSharpClassLibrary.Reflection
     {
         public int c { get; set; }
         public int d { get; set; }
-        public IList<Test> test { get; set; }
+        public IList<Rank> rank { get; set; }
     }
 
-    public class Test
+    public class Rank
     {
         public int e { get; set; }
         public int? f { get; set; }
-        
+        public IList<Price> price { get; set; }
+
     }
 
-    public struct GG
+    public class Price
     {
+        public int g { get; set; }
 
     }
 
@@ -45,54 +47,113 @@ namespace CSharpClassLibrary.Reflection
             var test = new ScoreCard()
             {
                 a = 4,
-                scoreSets = new List<ScoreSet>() 
-                    { 
-                        new ScoreSet() { b = 5 },
-                        new ScoreSet(), 
-                        new ScoreSet() { b = 3 },
-                        new ScoreSet() { b = 10 }
-                    },
-                rankSets = new List<RankSet>() 
-                    { 
+                scoreSets = new List<ScoreSet>()
+                {
+                    new ScoreSet() { b = 5 },
+                    new ScoreSet(),
+                    new ScoreSet() { b = 3 },
+                    new ScoreSet() { b = 10 }
+                },
+                rankSets = new List<RankSet>()
+                    {
                         new RankSet() { c = 12, d = 1 ,
-                            test = new List<Test>() { 
-                                        new Test() { e = 100 }, 
-                                        new Test() { e = 335 } 
+                            rank = new List<Rank>() {
+                                        new Rank() {
+                                            e = 100,
+                                            price = new List<Price>()
+                                            {
+                                                new Price(),
+                                                new Price(),
+                                                new Price()
+                                            }
+                                        }, 
+                                        new Rank() { 
+                                            e = 335,
+                                            price = new List<Price>()
+                                            {
+                                                new Price(),
+                                                new Price()
+                                            }
+                                        } 
                             } 
                         }, 
                         new RankSet() { c = 12, d = 2,
-                            test = new List<Test> { 
-                                        new Test() { e = 5 },
-                                        new Test() { e = 123 } 
+                            rank = new List<Rank> { 
+                                        new Rank() { 
+                                            e = 123,
+                                            price = new List<Price>()
+                                            {
+                                                new Price() { g = -24 },
+                                                new Price()
+                                            }
+                                        } 
                             } 
                         },
                         new RankSet() { c = 23, d = 22,
-                            test = new List<Test> { 
-                                        new Test() { e = 15 }, 
-                                        new Test() { e = 3435 }
+                            rank = new List<Rank> { 
+                                        new Rank() { 
+                                            e = 15,
+                                            price = new List<Price>()
+                                            {
+                                                new Price(),
+                                                new Price()
+                                            }
+                                        }, 
+                                        new Rank() { 
+                                            e = 3435,
+                                            price = new List<Price>()
+                                            {
+                                                new Price() { g = 10000 },
+                                                new Price() { g = -500 }
+                                            }
+                                        }
                             }
                         }
                     }
             };
 
-            //var test2 = new RankSet() { w = new Test4() };
+            var test2 = new RankSet() 
+            { 
+                c = 2, 
+                d = 3, 
+                rank = new List<Rank> {
+                    new Rank() {
+                        price = new List<Price>()
+                            {
+                                new Price() ,
+                                new Price()
+                            }
+                        }
+                }
+            };
+
+            List<ExpandoObject> csvPage = new();
+            //csvPage.AttachFlatableObject(test);
+
             int count = 0;
             IDictionary<string, int> Dict = new Dictionary<string, int>();
-            Show(test, ref count, Dict);
-            Console.WriteLine("============");
-            Console.WriteLine(count);
-            foreach (var pair in Dict)
+            csvPage.AttachObjectAsDecomposed(test);
+            //Console.WriteLine("============");
+            //Console.WriteLine($"Count: {count}");
+            //foreach (var keyValuePair in Dict)
+            //{
+            //    Console.WriteLine($"{keyValuePair.Key}: {keyValuePair.Value}");
+            //}
+            Console.WriteLine($"============================");
+            foreach (var expandoObject in csvPage)
             {
-                Console.WriteLine($"{pair.Key}: {pair.Value}");
+                Console.Write($"---\n");
+                foreach (var pair in expandoObject as IDictionary<string, object>)
+                {
+                    Console.WriteLine($"{pair.Key}: {pair.Value}");
+                }
+                Console.Write($"---");
             }
-            //List<ExpandoObject> csvPage = new ();
-            //csvPage.AttachFlatableObject(test);
-            //Console.WriteLine("test");
         }
 
         static void Show(object test, ref int count, IDictionary<string, int> Dict)
         {
-            foreach (var results in CartesianProduct(test))
+            foreach (var results in Decompose(test))
             {
                 foreach (var (propertyInfo, value) in results as IEnumerable<(PropertyInfo, object)>)
                 {
@@ -117,6 +178,114 @@ namespace CSharpClassLibrary.Reflection
             }
         }
 
+        static void AttachObjectAsDecomposed(this IList<ExpandoObject> csvPage, 
+            object composedObject, 
+            //ref int count*/, 
+            //IDictionary<string, int> Dict, 
+            IDictionary<string, object> csvRow = null,
+            IDictionary<string, object> tempRow = null)
+        {
+            if (composedObject.GetType().GetInterfaces().Any(
+                i => i.IsGenericType &&
+                i.GetGenericTypeDefinition() == typeof(IList<>)))
+            {
+                IList<object> enumerableObject = (IList<object>)composedObject;
+                for (int i =0; i < enumerableObject.Count; i++)
+                {
+                    csvPage.AttachObjectAsDecomposed(enumerableObject[i]);
+                }
+            }
+            else
+            {
+                foreach (var decomposedObjects in composedObject.Decompose())
+                {
+                    if (csvRow == null)
+                    {
+                        if (tempRow != null)
+                        {
+                            csvRow = new ExpandoObject();
+                            csvPage.Add(csvRow as ExpandoObject);
+                            foreach (var keyValuePair in tempRow)
+                            {
+                                csvRow.Add(keyValuePair.Key, keyValuePair.Value);
+                            }
+                        }
+                        else
+                        {
+                            csvRow = new ExpandoObject();
+                            csvPage.Add(csvRow as ExpandoObject);
+                        }
+                    }
+                    foreach (var (propertyInfo, value) in decomposedObjects as IEnumerable<(PropertyInfo propertyInfo, object value)>)
+                    {
+                        if (propertyInfo.PropertyType.IsValueType)
+                        {
+                            Console.WriteLine($"{propertyInfo.Name}: {value}, ");
+                            if (!csvRow.TryAdd(propertyInfo.Name, value))
+                            {
+                                csvRow[propertyInfo.Name] = value;
+                            }
+                            //count++;
+                            //if (Dict.TryGetValue(propertyInfo.Name, out int propertyInfoCount))
+                            //{
+                            //    Dict[propertyInfo.Name] = ++propertyInfoCount;
+                            //}
+                            //else
+                            //{
+                            //    Dict.Add(propertyInfo.Name, 1);
+                            //}
+                        }
+                        else
+                        {
+                            csvPage.AttachObjectAsDecomposed(value, /*ref count, Dict,*/ csvRow, tempRow);
+                        }
+                    }
+                    tempRow = csvRow;
+                    csvRow = null;
+                }
+            }
+            
+        }
+
+        static void AttachFlatableObject2(this IList<ExpandoObject> csvPage,
+           object test, ref int count,
+           IDictionary<string, int> Dict,
+           IDictionary<string, object> csvRow = null)
+        {
+            foreach (var results in Decompose(test))
+            {
+                if (csvRow == null)
+                {
+                    csvRow = new ExpandoObject();
+                    csvPage.Add(csvRow as ExpandoObject);
+                }
+                foreach (var (propertyInfo, value) in results as IEnumerable<(PropertyInfo, object)>)
+                {
+                    if (propertyInfo.PropertyType.IsValueType)
+                    {
+                        Console.WriteLine($"{propertyInfo.Name}: {value}, ");
+                        if (!csvRow.TryAdd(propertyInfo.Name, value))
+                        {
+
+                        }
+                        count++;
+                        if (Dict.TryGetValue(propertyInfo.Name, out int propertyInfoCount))
+                        {
+                            Dict[propertyInfo.Name] = ++propertyInfoCount;
+                        }
+                        else
+                        {
+                            Dict.Add(propertyInfo.Name, 1);
+                        }
+                    }
+                    else
+                    {
+                        csvPage.AttachFlatableObject2(value, ref count, Dict, csvRow);
+                    }
+                }
+                csvRow = null;
+            }
+        }
         static void AttachFlatableObject(this IList<ExpandoObject> csvPage, object layeredObject, IDictionary<string, object> csvRow = null)
         {
             if (layeredObject.GetType()
@@ -227,11 +396,12 @@ namespace CSharpClassLibrary.Reflection
             }
             return (propertyInfo.GetValue(original) as IEnumerable<object>).GetEnumerator();
         }
-        static IEnumerable<object> CartesianProduct(object original)
+        static IEnumerable<object> Decompose(this object composedObject)
         {
-            var propertiesInfo = original.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            //CartesainProduct
+            var propertiesInfo = composedObject.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
             var IObjectEnumeratorTupleArray = propertiesInfo
-                  .Select(propertyInfo => (PropertyInfo: propertyInfo, Enumerator: GetObjectEnumerator(propertyInfo, original)))
+                  .Select(propertyInfo => (PropertyInfo: propertyInfo, Enumerator: GetObjectEnumerator(propertyInfo, composedObject)))
                   .Where(IObjectEnumeratorTuple => IObjectEnumeratorTuple.Enumerator.MoveNext())
                   .ToArray();
             while (true)
@@ -261,7 +431,6 @@ namespace CSharpClassLibrary.Reflection
                 }
             }
         }
-
         static IEnumerable<object> OfCartesianProductOf(this IEnumerable<PropertyInfo> propertiesInfo, object original)
         {
             var IObjectEnumeratorTupleArray = propertiesInfo
@@ -296,7 +465,6 @@ namespace CSharpClassLibrary.Reflection
                 }
             }
         }
-
         public static IEnumerable<IEnumerable<T>> CartesianProduct<T>(this IEnumerable<IEnumerable<T>> sequences)
         {
             if (sequences == null)
@@ -312,13 +480,11 @@ namespace CSharpClassLibrary.Reflection
                     accseq => sequence,
                     (accseq, item) => accseq.Concat(new[] { item })));
         }
-
         public static IEnumerable Flatten(params object[] objects)
         {
             // Can't easily get varargs behaviour with IEnumerable
             return Flatten((IEnumerable)objects);
         }
-
         public static IEnumerable Flatten(IEnumerable enumerable)
         {
             foreach (object element in enumerable)
@@ -334,6 +500,25 @@ namespace CSharpClassLibrary.Reflection
                 }
             }
         }
+        public static IEnumerable<T> Flatten<T>(
+        this IEnumerable<T> items,
+        Func<T, IEnumerable<T>> getChildren)
+        {
+            var stack = new Stack<T>();
+            foreach (var item in items)
+                stack.Push(item);
 
+            while (stack.Count > 0)
+            {
+                var current = stack.Pop();
+                yield return current;
+
+                var children = getChildren(current);
+                if (children == null) continue;
+
+                foreach (var child in children)
+                    stack.Push(child);
+            }
+        }
     }
 }
