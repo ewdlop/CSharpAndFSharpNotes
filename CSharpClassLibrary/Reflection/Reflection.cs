84e1712a4c3a1b2effe6e4244710f8e92700094a
Raymond Lei <Raymond.Lei@darkmatter2bd.com>
2021-11-05 00:10:59 -0400

    changes

--- CSharpClassLibrary/Reflection/Reflection.cs
+++ CSharpClassLibrary/Reflection/Reflection.cs
@@ -9,28 +9,27 @@ namespace CSharpClassLibrary.Reflection
 {
     public class ScoreCard
     {
-        public int x { get; set; }
-        public IList<ScoreSet> dd { get; set; }
-        public IList<ScoreSet> z { get; set; }
-        public IList<RankSet> n { get; set; }
+        public int a { get; set; }
+        public IList<ScoreSet> scoreSets { get; set; }
+        public IList<RankSet> rankSets { get; set; }
     }
 
     public class ScoreSet
     {
-        public int y { get; set; }
+        public int b { get; set; }
     }
 
     public class RankSet
     {
-        public int s { get; set; }
-        public int ss { get; set; }
-        public IList<Test4> w { get; set; }
+        public int c { get; set; }
+        public int d { get; set; }
+        public IList<Test> test { get; set; }
     }
 
-    public class Test4
+    public class Test
     {
-        public int bb { get; set; }
-        public int? test { get; set; }
+        public int e { get; set; }
+        public int? f { get; set; }
         
     }
 
@@ -45,46 +44,77 @@ namespace CSharpClassLibrary.Reflection
         {
             var test = new ScoreCard()
             {
-                x = 4,
-                dd = new List<ScoreSet>() 
+                a = 4,
+                scoreSets = new List<ScoreSet>() 
                     { 
-                        new ScoreSet() { y = 5 },
+                        new ScoreSet() { b = 5 },
                         new ScoreSet(), 
-                        new ScoreSet() { y = 3 },
-                        new ScoreSet() { y = 10 }
+                        new ScoreSet() { b = 3 },
+                        new ScoreSet() { b = 10 }
                     },
-                n = new List<RankSet>() 
+                rankSets = new List<RankSet>() 
                     { 
-                        new RankSet() { s = 12, ss = 1 ,
-                            w = new List<Test4>() { 
-                                    new Test4() { bb = 100 }, 
-                                    new Test4() { bb = 335 } 
+                        new RankSet() { c = 12, d = 1 ,
+                            test = new List<Test>() { 
+                                        new Test() { e = 100 }, 
+                                        new Test() { e = 335 } 
                             } 
                         }, 
-                        new RankSet() { s = 12, ss = 2,
-                            w = new List<Test4> { 
-                                    new Test4() { bb = 5 },
-                                    new Test4() { bb = 123 } 
+                        new RankSet() { c = 12, d = 2,
+                            test = new List<Test> { 
+                                        new Test() { e = 5 },
+                                        new Test() { e = 123 } 
                             } 
                         },
-                        new RankSet() { s = 23, ss = 22,
-                            w = new List<Test4> { 
-                                    new Test4() { bb = 15 }, 
-                                    new Test4() { bb = 3435 }
+                        new RankSet() { c = 23, d = 22,
+                            test = new List<Test> { 
+                                        new Test() { e = 15 }, 
+                                        new Test() { e = 3435 }
                             }
                         }
-                    },
-                z = new List<ScoreSet>()
-                {
-
-                }
+                    }
             };
 
             //var test2 = new RankSet() { w = new Test4() };
+            int count = 0;
+            IDictionary<string, int> Dict = new Dictionary<string, int>();
+            Show(test, ref count, Dict);
+            Console.WriteLine("============");
+            Console.WriteLine(count);
+            foreach (var pair in Dict)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value}");
+            }
+            //List<ExpandoObject> csvPage = new ();
+            //csvPage.AttachFlatableObject(test);
+            //Console.WriteLine("test");
+        }
 
-            List<ExpandoObject> csvPage = new ();
-            csvPage.AttachFlatableObject(test);
-            Console.WriteLine("test");
+        static void Show(object test, ref int count, IDictionary<string, int> Dict)
+        {
+            foreach (var results in CartesianProduct(test))
+            {
+                foreach (var (propertyInfo, value) in results as IEnumerable<(PropertyInfo, object)>)
+                {
+                    if (propertyInfo.PropertyType.IsValueType)
+                    {
+                        Console.WriteLine($"{propertyInfo.Name}: {value}, ");
+                        count++;
+                        if (Dict.TryGetValue(propertyInfo.Name, out int propertyInfoCount))
+                        {
+                            Dict[propertyInfo.Name] = ++propertyInfoCount;
+                        }
+                        else
+                        {
+                            Dict.Add(propertyInfo.Name, 1);
+                        }
+                    }
+                    else
+                    {
+                        Show(value, ref count, Dict);
+                    }
+                }
+            }
         }
 
         static void AttachFlatableObject(this IList<ExpandoObject> csvPage, object layeredObject, IDictionary<string, object> csvRow = null)
@@ -94,9 +124,9 @@ namespace CSharpClassLibrary.Reflection
                         .Any(p => p.PropertyType.IsGenericType && p.PropertyType.GetGenericTypeDefinition() == typeof(IList<>)))
             {
                 var IListProperties = layeredObject.GetType()
-                                                .GetProperties()
-                                                .Where(p => p.PropertyType.IsGenericType &&
-                                                       p.PropertyType.GetGenericTypeDefinition() == typeof(IList<>));
+                                                 .GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                                                //.Where(p => p.PropertyType.IsGenericType &&
+                                                //       p.PropertyType.GetGenericTypeDefinition() == typeof(IList<>));
 
                 foreach (var product in IListProperties.OfCartesianProductOf(layeredObject))
                 {
@@ -107,7 +137,7 @@ namespace CSharpClassLibrary.Reflection
                     }
                     foreach (var (EnumerablePropertyInfo, Current) in product as IEnumerable<(PropertyInfo, object)>)
                     {
-                        
+                        Console.WriteLine(EnumerablePropertyInfo.Name);
                         if (Current.GetType()
                             .GetProperties(BindingFlags.Public | BindingFlags.Instance)
                             .Any(p => p.PropertyType.IsGenericType &&
@@ -125,8 +155,8 @@ namespace CSharpClassLibrary.Reflection
                             {
                                 if(CurrentNonIListProperties[i].PropertyType.IsValueType)
                                 {
+                                    Console.WriteLine(CurrentNonIListProperties[i].Name);
                                     csvRow.Add(CurrentNonIListProperties[i].Name, CurrentNonIListProperties[i].GetValue(Current));
-
                                 }
                                 else
                                 {
@@ -144,6 +174,7 @@ namespace CSharpClassLibrary.Reflection
                     {
                         if (nonIListProperties[i].PropertyType.IsValueType)
                         {
+                            Console.WriteLine(nonIListProperties[i].Name);
                             csvRow.Add(nonIListProperties[i].Name, nonIListProperties[i].GetValue(layeredObject));
                         }
                         else
@@ -185,6 +216,49 @@ namespace CSharpClassLibrary.Reflection
                     }
                 }
                 csvRow = null;
+            }
+        }
+        static IEnumerator Test(PropertyInfo propertyInfo, object original)
+        {
+            if(propertyInfo.GetValue(original) as IEnumerable<object> is null)
+            {
+                var test = new object[] { propertyInfo.GetValue(original) };
+                return test.AsEnumerable().GetEnumerator();
+            }
+            return (propertyInfo.GetValue(original) as IEnumerable<object>).GetEnumerator();
+        }
+        static IEnumerable<object> CartesianProduct(object original)
+        {
+            var propertiesInfo = original.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var IObjectEnumeratorTupleArray = propertiesInfo
+                  .Select(propertyInfo => (PropertyInfo: propertyInfo, Enumerator: Test(propertyInfo, original)))
+                  .Where(IObjectEnumeratorTuple => IObjectEnumeratorTuple.Enumerator.MoveNext())
+                  .ToArray();
+            while (true)
+            {
+                // yield current values
+                yield return IObjectEnumeratorTupleArray
+                    .Select(IObjectEnumeratorTuple => (IObjectEnumeratorTuple.PropertyInfo, IObjectEnumeratorTuple.Enumerator.Current));
+
+                // increase enumerators
+                foreach (var IObjectEnumeratorTuple in IObjectEnumeratorTupleArray)
+                {
+                    // reset the slot if it couldn't move next
+                    if (!IObjectEnumeratorTuple.Enumerator.MoveNext())//move next has side effect!!!(it moves then check)
+                    {
+                        // stop when the last enumerator resets
+                        if (IObjectEnumeratorTuple == IObjectEnumeratorTupleArray.Last())
+                        {
+                            yield break; //this exit the loop
+                        }
+                        IObjectEnumeratorTuple.Enumerator.Reset();
+                        IObjectEnumeratorTuple.Enumerator.MoveNext();
+                        // move to the next enumerator if this reseted
+                        continue;
+                    }
+                    // we could increase the current enumerator without reset so stop here
+                    break;
+                }
             }
         }
 
--- CSharpConsoleApp/Program.cs
+++ CSharpConsoleApp/Program.cs
@@ -18,14 +18,14 @@ namespace CSharpConsoleApp
 
             //Console.WriteLine($"Method from native class: {Test(2)}");
 
-            int productOfTwo = Number.productOfTwo(1, 2);
-            Console.WriteLine(string.Format("{0}", productOfTwo));
-            int sumOfThree = Number.sumOfThree(1, 2, 3);
-            Console.WriteLine(string.Format("{0}", sumOfThree));
+            //int productOfTwo = Number.productOfTwo(1, 2);
+            //Console.WriteLine(string.Format("{0}", productOfTwo));
+            //int sumOfThree = Number.sumOfThree(1, 2, 3);
+            //Console.WriteLine(string.Format("{0}", sumOfThree));
 
-            var bits = new[] { false, true, false };
-            using var sim = new QuantumSimulator();
-            Console.WriteLine($"Input: {bits.ToDelimitedString()}");
+            //var bits = new[] { false, true, false };
+            //using var sim = new QuantumSimulator();
+            //Console.WriteLine($"Input: {bits.ToDelimitedString()}");
 
             //var restored = await RunAlgorithm.Run(sim, new QArray<bool>(bits));
             //Console.WriteLine($"Output: {restored.ToDelimitedString()}");
@@ -35,28 +35,28 @@ namespace CSharpConsoleApp
             //PeekableEnumerableAdapter<char> it2 = new PeekableEnumerableAdapter<char>("true abc");
             //Token token1 = Token.ParsingUsingIterator(it1);
 
-            Memory<char> memory2 = new char[64];
-            IMemoryOwner<char> owner = MemoryPool<char>.Shared.Rent();
-            Console.Write("Enter a number: ");
-            try
-            {
-                var value = Int32.Parse(Console.ReadLine());
-                var memory = owner.Memory;
-                Memory.WriteInt32ToBuffer(value, memory);
-                Memory.DisplayBufferToConsole(owner.Memory.Slice(0, value.ToString().Length));
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("You did not enter a valid number.");
-            }
-            catch (OverflowException)
-            {
-                Console.WriteLine($"You entered a number less than {Int32.MinValue:N0} or greater than {Int32.MaxValue:N0}.");
-            }
-            finally
-            {
-                owner?.Dispose();
-            }
+            //Memory<char> memory2 = new char[64];
+            //IMemoryOwner<char> owner = MemoryPool<char>.Shared.Rent();
+            //Console.Write("Enter a number: ");
+            //try
+            //{
+            //    var value = Int32.Parse(Console.ReadLine());
+            //    var memory = owner.Memory;
+            //    Memory.WriteInt32ToBuffer(value, memory);
+            //    Memory.DisplayBufferToConsole(owner.Memory.Slice(0, value.ToString().Length));
+            //}
+            //catch (FormatException)
+            //{
+            //    Console.WriteLine("You did not enter a valid number.");
+            //}
+            //catch (OverflowException)
+            //{
+            //    Console.WriteLine($"You entered a number less than {Int32.MinValue:N0} or greater than {Int32.MaxValue:N0}.");
+            //}
+            //finally
+            //{
+            //    owner?.Dispose();
+            //}
 
             //using (var owner2 = MemoryPool<char>.Shared.Rent())
             //{
