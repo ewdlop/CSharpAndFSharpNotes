fccabd63c2c3888e32bf300c7a9e378fbaf07149
ewdlop <ray810815@gmail.com>
2021-11-16 00:43:44 -0500

    changes

--- DontRunMe/Program.cs
+++ DontRunMe/Program.cs
@@ -1,30 +1,34 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection;
+
 Console.WriteLine("Hello, World!");
 
-string filename = "CSharpAndFSharp/DontRunMe.cs"; //read in file to run from user
-try
+string filename = $"{AssemblyDirectory}/DontRunMe.exe";
 {
     if (FourtyTwo(filename, args))
         for (; ; );
     else
         return;
 }
-catch (OverflowException)
+catch (Win32Exception)
 {
     for (; ; );
 }
 
 bool FourtyTwo(string filename, string[] args)
 {
-    bool noanswer = true;
-    int i = 0;
-    while(noanswer)
-    {
-        checked
-        {
-           i++;
-        }
-    }
+    var process = Process.Start(filename, args);
+    process.WaitForExit();
     return true;
+}
+
+string? AssemblyDirectory()
+{
+    string codeBase = Assembly.GetExecutingAssembly().Location;
+    UriBuilder uri = new UriBuilder(codeBase);
+    string path = Uri.UnescapeDataString(uri.Path);
+    return Path.GetDirectoryName(path);
 }
