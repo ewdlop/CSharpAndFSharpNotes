f9749e1e0443d43f034e4ad571b7d3c199dab043
ewdlop <ray810815@gmail.com>
2021-11-18 22:23:20 -0500

    changes

---
+++ CSharpClassLibrary/PythonInterop/IronPython/PythonCall.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharpClassLibrary.PythonInterop.IronPython
+{
+    public class Python
+    {
+        //static void Call(string[] args)
+        //{
+        //    Microsoft.Scripting.Hosting.ScriptEngine pythonEngine =
+        //        IronPython.Hosting.Python.CreateEngine();
+        //    Microsoft.Scripting.Hosting.ScriptSource pythonScript =
+        //        pythonEngine.CreateScriptSourceFromString("print 'Hello World!'");
+        //    pythonScript.Execute();
+        //}
+    }
+}
