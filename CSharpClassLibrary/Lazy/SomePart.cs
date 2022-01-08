98d2ada04071e541ad57106fc91128423be8f011
ewdlop <ray810815@gmail.com>
2022-01-07 22:56:40 -0500

    changes

---
+++ CSharpClassLibrary/Lazy/SomePart.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpClassLibrary.Lazy
+{
+    public class SomePart : Lazy<Task<SlowPart>>
+    {
+        public SomePart(OtherPart eagerPart, Func<Task<SlowPart>> lazyPartFactory)
+            : base(() => Task.Run(lazyPartFactory))
+        {
+            EagerPart = eagerPart;
+        }
+
+        public OtherPart EagerPart { get; }
+        public TaskAwaiter<SlowPart> GetAwaiter() => Value.GetAwaiter();
+    }
+
+    public class OtherPart
+    {
+    }
+
+    public class SlowPart
+    {
+    }
+}
