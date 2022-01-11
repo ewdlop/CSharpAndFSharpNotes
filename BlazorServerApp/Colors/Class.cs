3d7798b64df4f6001bdf0172ea2f7825206d350a
ewdlop <ray810815@gmail.com>
2022-01-11 00:20:45 -0500

    changes

---
+++ BlazorServerApp/Colors/Class.cs
@@ -0,0 +1,36 @@
+﻿using BlazorServerApp.Colors;
+using MediatR;
+
+namespace BlazorServerApp.Colors
+{
+    public class UpdateColorCommand : INotification
+    {
+        public string Color { get; set; }
+    }
+    public class UpdateColorEventArgs : EventArgs
+    {
+        public string Color { get; set; }
+    }
+}
+namespace BlazorServerApp.Pages
+{
+    public partial class Counter : INotificationHandler<UpdateColorCommand>
+    {
+        public static event EventHandler<UpdateColorEventArgs> ColorChanged;
+        public async Task Handle(UpdateColorCommand notification, CancellationToken cancellationToken)
+        {
+            ColorChanged?.Invoke(this, new UpdateColorEventArgs { Color = notification.Color });
+        }
+    }
+}
+namespace BlazorServerApp.Shared
+{
+    public partial class MainLayout : INotificationHandler<UpdateColorCommand>
+    {
+        public static event EventHandler<UpdateColorEventArgs> ColorChanged;
+        public async Task Handle(UpdateColorCommand notification, CancellationToken cancellationToken)
+        {
+            ColorChanged?.Invoke(this, new UpdateColorEventArgs { Color = notification.Color });
+        }
+    }
+}
