f8417514f79d4395409877ed4d73469e76d71d2e
ewdlop <ray810815@gmail.com>
2022-01-23 00:59:06 -0500

    changes

---
+++ FSharpClassLibrary/QuickSort.fs
@@ -0,0 +1,8 @@
+﻿module QuickSort
+
+let rec quicksort2 list =
+    match list with
+    | [] -> []                         
+    | first::rest -> 
+         let smaller,larger = List.partition ((>=) first) rest 
+         List.concat [quicksort2 smaller; [first]; quicksort2 larger]
