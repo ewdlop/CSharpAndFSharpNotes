f579245a8cf312eec4acd0eb37bba42b05b883e3
ewdlop <ray810815@gmail.com>
2022-04-25 00:04:00 -0400

    llvm stuff

--- LLVMApp/Program.cs
+++ LLVMApp/Program.cs
@@ -3,42 +3,44 @@ using LLVMApp.LLVM;
 using LLVMApp.Parsers;
 using LLVMSharp;
 using LLVMSharp.Interop;
+using System.Text;
 
 Run();
 unsafe static void Run()
 {
     //https://llvm.org/docs/_images/MCJIT-engine-builder.png
-    sbyte jit = Convert.ToSByte("JIT");
-    LLVMModuleRef module = LLVM.ModuleCreateWithName(&jit);
-    LLVMBuilderRef builder = LLVM.CreateBuilder();
-
-    LLVM.LinkInMCJIT(); //https://llvm.org/docs/MCJITDesignAndImplementation.html
-    LLVM.InitializeX86TargetInfo();
-    LLVM.InitializeX86Target();
-    LLVM.InitializeX86TargetMC();
-    LLVMOpaqueExecutionEngine* engine;
-    sbyte* errorMessage;
-    if (LLVM.CreateExecutionEngineForModule(&engine, module, &errorMessage) == 1)
+    sbyte[] bytes = (sbyte[])(Array)Encoding.ASCII.GetBytes("Convert");
+    fixed(sbyte* ptr = bytes)
     {
-        Console.WriteLine(Convert.ToString(*errorMessage));
-        // LLVM.DisposeMessage(errorMessage);
-        return;
+        LLVMModuleRef module = LLVM.ModuleCreateWithName(ptr);
+        LLVMBuilderRef builder = LLVM.CreateBuilder();
+
+        LLVM.LinkInMCJIT(); //https://llvm.org/docs/MCJITDesignAndImplementation.html
+        LLVM.InitializeX86TargetInfo();
+        LLVM.InitializeX86Target();
+        LLVM.InitializeX86TargetMC();
+        LLVMOpaqueExecutionEngine* engine;
+        sbyte* errorMessage;
+        if (LLVM.CreateExecutionEngineForModule(&engine, module, &errorMessage) == 1)
+        {
+            Console.WriteLine(errorMessage->ToString());
+            // LLVM.DisposeMessage(errorMessage);
+            return;
+        }
+        //IParserListener codeGenlistener = new CodeGenParserListener(engine, passManager, new CodeGenVisitor(module, builder));
+        //var binaryOperatorPrecedence = new Dictionary<char, int>
+        //{
+        //    ['<'] = 10,
+        //    ['+'] = 20,
+        //    ['-'] = 20,
+        //    ['*'] = 40
+        //};
+
+        //ILexer scanner = new Lexer(Console.In, binaryOperatorPrecedence);
+        //IParser parser = new Parser(scanner, codeGenlistener);
+
+        //MainLoop(scanner,parser);
     }
-
-    //IParserListener codeGenlistener = new CodeGenParserListener(engine, passManager, new CodeGenVisitor(module, builder));
-    //var binaryOperatorPrecedence = new Dictionary<char, int>
-    //{
-    //    ['<'] = 10,
-    //    ['+'] = 20,
-    //    ['-'] = 20,
-    //    ['*'] = 40
-    //};
-
-    //ILexer scanner = new Lexer(Console.In, binaryOperatorPrecedence);
-    //IParser parser = new Parser(scanner, codeGenlistener);
-
-    //MainLoop(scanner,parser);
-
 }
 
 
