ba4a03503e78722f9a702ae49f7404b1d724263b
ewdlop <ray810815@gmail.com>
2022-06-25 23:21:05 -0400

    dont know what I am doing

---
+++ CSharpClassLibrary/FP/Future.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CSharpClassLibrary.FP
+{
+    public class Future<T>
+    {
+        public Task<T> Value { get; private set; }
+
+        public Future(T value) => Value = Task.FromResult(value);
+
+        public Future(Task<T> task) => Value = task;
+
+        public Future<U> Bind<U>(Func<T, Future<U>> func) => 
+            new(Value.ContinueWith(t => func(t.Result).Value).Unwrap());
+
+        public void OnComplete(Action<T> action)
+        {
+            Value.ContinueWith(t => action(t.Result));
+        }
+    }
+
+    public static partial class GenericFutureExtension
+    {
+        public static Future<T> Unit<T>(this T value) => new(value);
+    }
+
+
+}
