bf26427bdf86f7153a8f98e12feaebaa9e1f8cd1
ewdlop <ray810815@gmail.com>
2023-09-14 10:30:05 -0400

    Azure Function Triggers review notes

---
+++ FunctionApp2/Function1.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Threading;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionApp2
+{
+    public class Function1
+    {
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> log)
+        {
+            _logger = log;
+        }
+
+        [FunctionName("Function1")]
+        public void Run([ServiceBusTrigger("mytopic", "mysubscription", Connection = "test")]string mySbMsg)
+        {
+            _logger.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
+        }
+
+        [FunctionName("ServiceBusQueueTriggerCSharp")]
+        public static void Run(
+            [ServiceBusTrigger("myqueue", Connection = "ServiceBusConnection")]
+            string myQueueItem,
+            Int32 deliveryCount,
+            DateTime enqueuedTimeUtc,
+            string messageId,
+            ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            log.LogInformation($"EnqueuedTimeUtc={enqueuedTimeUtc}");
+            log.LogInformation($"DeliveryCount={deliveryCount}");
+            log.LogInformation($"MessageId={messageId}");
+        }
+    }
+    }
+}
