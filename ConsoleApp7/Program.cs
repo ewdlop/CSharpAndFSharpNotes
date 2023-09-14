72e1ae65d74f7a1d4836af59407af9904bf0ce6a
ewdlop <ray810815@gmail.com>
2023-09-14 02:57:35 -0400

    Interop code with WindowsAzure.ServiceBus

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -4,10 +4,12 @@ using Azure.Identity;
 using Azure.Messaging.ServiceBus;
 using Azure.Messaging.ServiceBus.Administration;
 using System.Net;
+using System.Runtime.Serialization;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.Json;
 using System.Transactions;
-
+using System.Xml;
 {
     string fullyQualifiedNamespace = "yournamespace.servicebus.windows.net";
     await using ServiceBusClient client = new ServiceBusClient(fullyQualifiedNamespace, new DefaultAzureCredential());
@@ -480,4 +482,53 @@ using System.Transactions;
 
     //Delete a topic
     await client.DeleteTopicAsync(topicName);
+
+    string queueName = string.Empty;
+    ServiceBusClient serviceBusClient = new ServiceBusClient(connectionString);
+    //Sending a message using Azure.Messaging.ServiceBus that will be received with WindowsAzure.ServiceBus
+    {
+        ServiceBusSender sender = serviceBusClient.CreateSender(queueName);
+        // When constructing the `DataContractSerializer`, We pass in the type for the model, which can be a strongly typed model or some pre-serialized data.
+        // If you use a strongly typed model here, the model properties will be serialized into XML. Since JSON is more commonly used, we will use it in our example, and
+        // and specify the type as string, since we will provide a JSON string.
+        var serializer = new DataContractSerializer(typeof(string));
+        using var stream = new MemoryStream();
+        XmlDictionaryWriter writer = XmlDictionaryWriter.CreateBinaryWriter(stream);
+
+        // serialize an instance of our type into a JSON string
+        string json = JsonSerializer.Serialize(new TestModel { A = "Hello world", B = 5, C = true });
+
+        // serialize our JSON string into the XML envelope using the DataContractSerializer
+        serializer.WriteObject(writer, json);
+        writer.Flush();
+
+        // construct the ServiceBusMessage using the DataContract serialized JSON
+        var message = new ServiceBusMessage(stream.ToArray());
+
+        await sender.SendMessageAsync(message);
+    }
+
+    {
+        ServiceBusReceiver receiver = serviceBusClient.CreateReceiver(queueName);
+        ServiceBusReceivedMessage received = await receiver.ReceiveMessageAsync();
+
+        // Similar to the send scenario, we still rely on the DataContractSerializer and we use string as our type because we are expecting a JSON
+        // message body.
+        var deserializer = new DataContractSerializer(typeof(string));
+        XmlDictionaryReader reader =
+            XmlDictionaryReader.CreateBinaryReader(received.Body.ToStream(), XmlDictionaryReaderQuotas.Max);
+
+        // deserialize the XML envelope into a string
+        string receivedJson = (string)deserializer.ReadObject(reader);
+
+        // deserialize the JSON string into TestModel
+        TestModel? output = JsonSerializer.Deserialize<TestModel>(receivedJson);
+    }
+}
+
+public class TestModel
+{
+    public string A { get; set; }
+    public int B { get; set; }
+    public bool C { get; set; }
 }
