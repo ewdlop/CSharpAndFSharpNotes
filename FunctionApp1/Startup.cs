941dd2708680d40e4a2cdc73e1c8fc108df5b9f6
ewdlop <ray810815@gmail.com>
2023-09-15 08:38:37 -0400

    DI for funcitonaApp and SIngleton CosmosClient

---
+++ FunctionApp1/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+
+[assembly: FunctionsStartup(typeof(FunctionApp1.Startup))]
+
+
+namespace FunctionApp1
+{
+    /// <summary>
+    /// https://devblogs.microsoft.com/cosmosdb/httpclientfactory-cosmos-db-net-sdk/
+    /// </summary>
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+            builder.Services.AddSingleton(serviceProvider =>
+            {
+                IHttpClientFactory httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+                CosmosClientOptions cosmosClientOptions = new CosmosClientOptions()
+                {
+                    HttpClientFactory = httpClientFactory.CreateClient
+                };
+                return new CosmosClient("<connection-string>", cosmosClientOptions);
+            });
+        }
+    }
+}
